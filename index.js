ce2324f2372d29c41e256fd0a849501128e4a0a5
akashmishra0908 <112822976+akashmishra0908@users.noreply.github.com>
2023-11-02 18:28:09 +0530

    back-end completed

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const express=require("express");
+const cors=require("cors");
 const { connection } = require("./db");
+const { boardRouter } = require("./routes/board");
+const { taskRouter } = require("./routes/task");
+const { subRouter } = require("./routes/subtask");
 
 
 
 const app=express();
 
-
-
-
+app.use(cors())
 app.use(express.json());
 
 
 
+app.get('/',async(req,res)=>{
+    res.send('Welcome to kanban backend')
+})
 
-
-
-
-
-
-
-
-
-
+app.use('/boards',boardRouter)
+app.use('/tasks',taskRouter)
+app.use('/subtasks',subRouter)
 
 
 
